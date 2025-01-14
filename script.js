245fc3d7f27f4ded769658e524ecf430d3ff25a5
Tushar Kulkarni <142671474+tusharkulkarni02@users.noreply.github.com>
2025-01-14 09:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "ae64b6a6a7484f80b855c9bbe2dc2b44";
+const API_KEY = "";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
